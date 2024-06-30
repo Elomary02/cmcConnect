23013a2eee944f89cff34df9b72a28f9fb059817
ELHOUCINE ELOMARY <elhoucineelomary@gmail.com>
2024-06-30 02:25:30 +0100

    events fragments done

--- app/src/main/java/com/example/cmcconnect/repository/studentRepository/StudentRepository.kt
+++ app/src/main/java/com/example/cmcconnect/repository/studentRepository/StudentRepository.kt
@@ -3,6 +3,5 @@ package com.example.cmcconnect.repository.studentRepository
 import com.example.cmcconnect.model.RessourceDto
 
 interface StudentRepository {
-
     suspend fun getRecentResourcesForStudentGroup(idStudent: Int): List<RessourceDto>?
 }
